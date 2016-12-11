cc0f78df708a0d6f1f7a3e6b4841c418cc9a05c5
cckcheng <colley888@gmail.com>
2016-12-10 18:15:57 -0800

    test

--- Pedigree/src/com/ccdev/pedigree/Pedigree.java
+++ Pedigree/src/com/ccdev/pedigree/Pedigree.java
@@ -10,11 +10,12 @@ public class Pedigree {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        FamilyTreeBuilder ftb = new FamilyTreeBuilder();
+        FamilyTreeBuilder ftb = new FamilyTreeBuilder(1);
         if(ftb.processInput("cheng.txt")) {
             System.out.println("Well Done");
         } else {
             System.out.println("Failed");
+            System.out.println(ftb.getError());
         }
     }
     
